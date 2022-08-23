41ea9fecc853f1ff7eddc3543106d24fcfab457b
inno <khe796@gmail.com>
2022-08-24 02:44:57 +0900

    #11: open/close event 추가

--- src/components/SelectFilter/index.tsx
+++ src/components/SelectFilter/index.tsx
@@ -1,5 +1,7 @@
 import type { FC } from 'react';
 import type { ISelectFilter } from 'src/types/components';
+
+import { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import { TextField, InputAdornment, Autocomplete } from '@mui/material';
 import { OutlinedInputProps } from '@mui/material/OutlinedInput';
@@ -58,64 +60,79 @@ export const SelectFilter: FC<ISelectFilter> = ({
 	onOptionClick,
 	label,
 	StartIcon,
-}) => (
-	<CustomAutoComplete
-		id={id}
-		options={options}
-		renderOption={(props, option) => {
-			if (hasTagItem(option)) {
-				return (
-					<CustomList
-						{...props}
-						onClick={onOptionClick}
-						className={`ctt_text_14 ctt_regular ${props.className}`}
-						style={{
-							padding: '12px 16px',
-						}}
-						value={option.value}
-					>
-						{option.label}
-					</CustomList>
-				);
-			}
-		}}
-		componentsProps={{
-			paper: {
-				style: {
-					borderRadius: '32px',
-					background: '#ffffff',
-					boxShadow: '0px 12px 32px rgba(0, 0, 0, 0.16)',
-				},
-			},
-		}}
-		ListboxProps={{
-			style: {
-				padding: '8px',
-			},
-		}}
-		popupIcon={<CustomArrowDownIcon />}
-		renderInput={(params) => (
-			<TextField
-				{...params}
-				placeholder={label}
-				InputLabelProps={{
-					className: 'ctt_text_14 ctt_medium',
-					shrink: false,
-				}}
-				variant="outlined"
-				InputProps={
-					{
-						...params.InputProps,
-						className: 'ctt_text_14 ctt_medium',
-						startAdornment: (
-							<InputAdornment position="start">{StartIcon}</InputAdornment>
-						),
-					} as Partial<OutlinedInputProps>
+}) => {
+	const [open, setOpen] = useState(false);
+	return (
+		<CustomAutoComplete
+			id={id}
+			options={options}
+			open={open}
+			onOpen={() => {
+				setOpen(true);
+			}}
+			onClose={() => {
+				setOpen(false);
+			}}
+			renderOption={(props, option) => {
+				if (hasTagItem(option)) {
+					return (
+						<CustomList
+							{...props}
+							onClick={(e) => {
+								setOpen(false);
+								if (onOptionClick !== undefined) {
+									onOptionClick(e);
+								}
+							}}
+							className={`ctt_text_14 ctt_regular ${props.className}`}
+							style={{
+								padding: '12px 16px',
+							}}
+							value={option.value}
+						>
+							{option.label}
+						</CustomList>
+					);
 				}
-				fullWidth
-			/>
-		)}
-	/>
-);
+			}}
+			componentsProps={{
+				paper: {
+					style: {
+						borderRadius: '32px',
+						background: '#ffffff',
+						boxShadow: '0px 12px 32px rgba(0, 0, 0, 0.16)',
+					},
+				},
+			}}
+			ListboxProps={{
+				style: {
+					padding: '8px',
+				},
+			}}
+			popupIcon={<CustomArrowDownIcon />}
+			renderInput={(params) => (
+				<TextField
+					{...params}
+					placeholder={label}
+					InputLabelProps={{
+						className: 'ctt_text_14 ctt_medium',
+						shrink: false,
+					}}
+					variant="outlined"
+					InputProps={
+						{
+							...params.InputProps,
+							className: 'ctt_text_14 ctt_medium',
+							startAdornment: (
+								<InputAdornment position="start">{StartIcon}</InputAdornment>
+							),
+						} as Partial<OutlinedInputProps>
+					}
+					fullWidth
+				/>
+			)}
+		/>
+	);
+};
 
 export default SelectFilter;
