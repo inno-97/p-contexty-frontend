87df8c262460078427c500f11b6aaa7853f5b55e
inno <khe796@gmail.com>
2022-09-02 10:43:45 +0900

    #34: 임시 모달 완료

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,19 +1,29 @@
 import type { TypographyProps } from '@mui/material';
 import type { TNextPageWithLayout } from 'src/types/components';
 
-import { Box, Stack, TextField, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Box, Stack, TextField, Button, ButtonProps, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 import DefaultLayout from 'src/components/Layout/DefaultLayout';
 import { SubContentsLayer } from 'src/components/CustomLayer';
 import { Writing, Paragraph } from 'src/components/Contents/Writing';
 import Contents from 'src/components/Contents';
+import Dialog from 'src/components/Dialog';
 
-const SendButton = styled(Button)(({ theme }) => {
+const SendButton = styled(({ ...props }: ButtonProps) => (
+	<Button
+		{...props}
+		disableFocusRipple
+		disableElevation
+		className={props.className + ' ctt_text_14 ctt_medium'}
+	/>
+))(({ theme }) => {
 	return {
 		backgroundColor: theme.palette.grey[400],
-		padding: '20px 48px',
+		textTransform: 'none',
 		borderRadius: '8px',
+		padding: '20px 48px',
 	};
 });
 
@@ -26,6 +36,16 @@ const SendButtonText = styled(({ ...props }: TypographyProps) => (
 });
 
 const Contact: TNextPageWithLayout = () => {
+	const [open, setOpen] = useState(false);
+
+	const handleDialogClose = () => {
+		setOpen(false);
+	};
+
+	const handleDialogOpen = () => {
+		setOpen(true);
+	};
+
 	return (
 		<Box>
 			<SubContentsLayer>
@@ -55,11 +75,14 @@ const Contact: TNextPageWithLayout = () => {
 							type="contents"
 						/>
 					</Stack>
-					<SendButton disableRipple={true}>
+					<SendButton onClick={handleDialogOpen}>
 						<SendButtonText>보내기</SendButtonText>
 					</SendButton>
 				</Contents>
 			</SubContentsLayer>
+			<Dialog open={open} onClose={handleDialogClose}>
+				<Box margin={'30px 30px'}>서비스 준비중입니다.</Box>
+			</Dialog>
 		</Box>
 	);
 };
