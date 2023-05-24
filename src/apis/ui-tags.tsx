e49715cecde0a8220012120f1ef63e54dcb6cc6d
inno <khe796@gmail.com>
2023-05-24 15:27:43 +0900

    #87: getUITags, Throw Error Message 변경

--- src/apis/ui-tags.tsx
+++ src/apis/ui-tags.tsx
@@ -1,7 +1,7 @@
 export async function getUITags() {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ui/tags`);
 	if (!res.ok) {
-		throw new Error('getUITags Fetch Error');
+		throw new Error(`Failed to Fetch, getUITags status(${res.status})`);
 	}
 
 	return res.json();
